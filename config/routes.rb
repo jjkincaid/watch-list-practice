021516e678663fe44aec2754a43bbc5436b16c5b
Jesica Kincaid <jesica.j.kincaid@gmail.com>
2022-09-20 22:13:55 +0200

    added routes and controllers lists and bookmarks

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
+  root to: "lists#index"
+  resources :lists, except: [:edit, :update] do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # get "lists", to: "lists#index"
+  # get "lists/new", to: "lists#new"
+  # post "lists", to: "lists#create"
+  # get "lists/:id", to: "lists#show"
+
+
 
   # Defines the root path route ("/")
   # root "articles#index"
